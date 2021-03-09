7db2813ea03bd1079981b200d99d87a2475fcc01
Giovani Boff <giovanisouza15@gmail.com>
2021-03-09 18:23:44 -0300

    feat: implements heapsort

--- heapsort/src/main/java/edu/ifrs/vvs/App.java
+++ heapsort/src/main/java/edu/ifrs/vvs/App.java
@@ -4,10 +4,15 @@ package edu.ifrs.vvs;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        int arr[] = { 12, 11, 13, 5, 6, 7 };
+        // int n = arr.length;
+
+        HeapSort ob = new HeapSort();
+        ob.sort(arr);
+
+        System.out.println("Sorted array is");
+        ob.printArray(arr);
     }
 }
