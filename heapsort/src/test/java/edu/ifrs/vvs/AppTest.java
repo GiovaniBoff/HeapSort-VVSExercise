b782bfa6abf1a77c89df4a427d9d35a50ecceaee
Giovani Boff <giovanisouza15@gmail.com>
2021-03-09 18:48:26 -0300

    add tests heapsort

--- heapsort/src/test/java/edu/ifrs/vvs/AppTest.java
+++ heapsort/src/test/java/edu/ifrs/vvs/AppTest.java
@@ -1,20 +1,45 @@
 package edu.ifrs.vvs;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
+
+    private HeapSort HeapSort;
+
+    @Before
+    public void configTests() {
+        HeapSort = new HeapSort();
+    }
+
+    /**
+     * BubbleSort Test.
+     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void MustSortNumberPositives() {
+        int[] arrayToTest = { 1, 4, 2, 8, 7 };
+        HeapSort.sort(arrayToTest);
+        int[] expectedValue = { 1, 2, 4, 7, 8 };
+
+        assertArrayEquals(expectedValue, arrayToTest);
+    }
+
+    @Test
+    public void MustSortNumberNegatives() {
+        int[] arrayToTest = { -1, -4, -2, -8, -7 };
+        HeapSort.sort(arrayToTest);
+        int[] expectedValue = { -8, -7, -4, -2, -1 };
+
+        assertArrayEquals(expectedValue, arrayToTest);
     }
 }
